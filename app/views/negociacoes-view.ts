a52ad99bc178adfe5a2e5260a024bf556ef23808
Victor Barra <victorbarrapereira@gmail.com>
2023-06-19 13:21:03 -0300

    TypeScript parte 2 - Aula 2

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,13 +1,8 @@
 import { Negociacao } from "../models/negociacao";
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
-
-    private elemento: HTMLElement;
-
-    constructor(seletor: string){
-        this.elemento = document.querySelector(seletor);
-    }
+export class NegociacoesView extends View<Negociacoes> {
 
     template(model: Negociacoes): string{
         return `
@@ -34,11 +29,5 @@ export class NegociacoesView {
                 </tbody>
             </table>
         `
-    }
-
-    update(model: Negociacoes): void{
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
     }
 }
