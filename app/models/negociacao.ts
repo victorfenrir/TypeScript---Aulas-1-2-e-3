060f9f42fb86d1e996ca751e93249960f1b8c1c8
Victor Barra <victorbarrapereira@gmail.com>
2023-06-20 16:03:15 -0300

    TypeScript parte 2 - Aula 4

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,5 +1,4 @@
 export class Negociacao {
-
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
@@ -13,6 +12,14 @@ export class Negociacao {
     get data(): Date {
         const data = new Date(this._data.getTime());
         return data;
+    }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string){
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
 
